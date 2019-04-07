fe6f75792fef34cd07280f5f5bdd184e09df7305
Darren Tey <darrentey19@gmail.com>
2019-04-08 01:09:49 +0800

    added migrations

--- sellfast/__init__.py
+++ sellfast/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
-
+import os
 
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"/*":{"origins":"*"}})
 app.config['SECRET_KEY'] = 'thisissecret'
 
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/backend_sellfast'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
