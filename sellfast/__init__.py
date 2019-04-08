875faafc91b88cf0b479e9b30d9ce312b408f08a
Darren Tey <darrentey19@gmail.com>
2019-04-08 22:17:30 +0800

    change database url

--- sellfast/__init__.py
+++ sellfast/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/*":{"origins":"*"}})
 app.config['SECRET_KEY'] = 'thisissecret'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/backend_sellfast'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://whasfcwduprrmg:da9360d68e47379e46010f6bf5a87467af38c8bcc090fef8bb3d86688dc2ee0f@ec2-23-21-136-232.compute-1.amazonaws.com:5432/dbepq4s7mmbcns'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
